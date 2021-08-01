845969412faf0f9c7d2427c40c6d25e867da066d
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-08-01 07:47:43 -0300

    atualização de livro verbo put

--- scel-rest/src/main/java/com/fatec/scel/controller/LivroController.java
+++ scel-rest/src/main/java/com/fatec/scel/controller/LivroController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,35 +35,33 @@ public class LivroController {
 
 	@PostMapping("/v1/livros")
 	public ResponseEntity<Object> create(@RequestBody @Valid Livro livro, BindingResult result) {
-        ResponseEntity<Object> response = null;
+		ResponseEntity<Object> response = null;
 		if (result.hasErrors()) {
 			logger.info(">>>>>> 1. controller chamou servico save - erro detectado no bean");
 			response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		} else {
 			logger.info(">>>>>> 1. controller chamou servico save sem erro no bean validation");
 			Optional<Livro> umLivro = servico.consultaPorIsbn(livro.getIsbn());
-			if(umLivro.isPresent()) {
-				
-				response= ResponseEntity.badRequest().body("ja cadastrado");
-			}
-			else {
-				
+			if (umLivro.isPresent()) {
+
+				response = ResponseEntity.badRequest().body("ja cadastrado");
+			} else {
+
 				servico.save(livro);
 				response = new ResponseEntity<>(HttpStatus.CREATED);
 			}
-			
+
 		}
 
 		return response;
 	}
 
-	@CrossOrigin //desabilita o cors do spring security
+	@CrossOrigin // desabilita o cors do spring security
 	@GetMapping("/v1/livros")
 	public ResponseEntity<List<Livro>> consultaTodos() {
 		return ResponseEntity.ok().body(servico.consultaTodos());
 	}
 
-	
 	@GetMapping("v1/livros/{isbn}")
 	public ResponseEntity<Livro> findByIsbn(@PathVariable String isbn) {
 		logger.info(">>>>>> 1. controller chamou servico consulta por isbn => " + isbn);
@@ -74,15 +73,29 @@ public class LivroController {
 	@DeleteMapping("v1/livros/{isbn}")
 	public ResponseEntity<?> delete(@PathVariable String isbn) {
 		Optional<Livro> umLivro = servico.consultaPorIsbn(isbn);
-		if(umLivro.isPresent()) {
+		if (umLivro.isPresent()) {
 			logger.info(">>>>>> 1. controller chamou servico delete por isbn => " + isbn);
 			servico.delete(umLivro.get().getId());
 			return ResponseEntity.ok().build();
-		}
-		else {
+		} else {
 			logger.info(">>>>>> 3. controller chamou servico delete isbn nao localizado => " + isbn);
 			return ResponseEntity.notFound().build();
 		}
-		
+
+	}
+
+	@PutMapping("v1/livros")
+	public ResponseEntity<Livro> replaceLivro(@RequestBody Livro livro) {
+		Optional<Livro> umLivro = servico.consultaPorIsbn(livro.getIsbn());
+		if (umLivro.isPresent()) {
+			Livro l = umLivro.get();
+			l.setAutor(livro.getAutor());
+			l.setTitulo(livro.getTitulo());
+			servico.save(l);
+			return ResponseEntity.ok(l);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 }
